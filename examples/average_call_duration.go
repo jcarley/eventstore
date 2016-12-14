b748ae1576670f9bb17deb2184985f7a858dbbeb
jcarley <jcarley@gettyimages.com>
2016-12-14 11:01:52 -0600

    Adds an example projection

---
+++ examples/average_call_duration.go
@@ -0,0 +1,50 @@
+package examples
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/jcarley/eventstore"
+)
+
+type AverageFunc func(nextValue int, acc float32, size int) (float32, int)
+
+func (this AverageFunc) Apply(event DomainEvent) {
+}
+
+type AverageCallDuration struct {
+	TotalCalls        int
+	CallLengthTotal   float32
+	AverageCallLength float32
+	*eventstore.Aggregate
+}
+
+func NewAverageCallDuration() *AverageCallDuration {
+	return &AverageCallDuration{
+		TotalCalls:        0,
+		CallLengthTotal:   0,
+		AverageCallLength: 0,
+		Aggregate:         eventstore.NewAggregate(),
+	}
+}
+
+func (this *AverageCallDuration) Apply(event eventstore.DomainEvent) {
+	this.Aggregate.Apply(this, event)
+}
+
+func (this *AverageCallDuration) WhenPhoneCallCharged(phoneCallCharged PhoneCallCharged) {
+	this.TotalCalls++
+
+	startTime, _ := time.Parse(time.RFC3339, phoneCallCharged.StartTime)
+	endTime, _ := time.Parse(time.RFC3339, phoneCallCharged.EndTime)
+
+	elapsed := endTime.Sub(startTime)
+	lengthOfCallInMinutes := elapsed.Minutes()
+	this.CallLengthTotal += float32(lengthOfCallInMinutes)
+
+	this.AverageCallLength = this.CallLengthTotal / float32(this.TotalCalls)
+}
+
+func (this *AverageCallDuration) String() string {
+	return fmt.Sprintf("%.2f", this.AverageCallLength)
+}
--- examples/pay_as_you_go_account_test.go
+++ examples/pay_as_you_go_account_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/jcarley/eventstore"
 	. "github.com/onsi/gomega"
 )
 
@@ -21,7 +22,7 @@ func TestCallingApply(t *testing.T) {
 	account.IncreaseCreditLine(5)
 	fmt.Printf("%+v\n\n", account)
 
-	startTime, endTime := MakeCall()
+	startTime, endTime := MakeCall(15)
 	account.CallCompleted(startTime, endTime)
 	fmt.Printf("%+v\n\n", account)
 
@@ -38,14 +39,45 @@ func TestCallingApply(t *testing.T) {
 		account2.Apply(change)
 	}
 	fmt.Printf("%+v\n\n", account2)
+	fmt.Printf("%+v\n\n", account2.Changes())
 	fmt.Printf("Version: %d\n\n", account2.Version())
 }
 
-func MakeCall() (time.Time, time.Time) {
-	lengthOfCall, _ := time.ParseDuration("15m")
+func TestProjectionApply(t *testing.T) {
+
+	events := []eventstore.DomainEvent{
+		NewPhoneCallCharged(15),
+		NewPhoneCallCharged(10),
+		NewPhoneCallCharged(25),
+		NewPhoneCallCharged(65),
+		CreditAdded{6},
+		NewPhoneCallCharged(23),
+		NewPhoneCallCharged(10),
+		NewPhoneCallCharged(38),
+	}
+
+	projection := NewAverageCallDuration()
+	for _, event := range events {
+		projection.Apply(event)
+	}
+
+	fmt.Printf("\nThe total calls calculated: %d\n", projection.TotalCalls)
+	fmt.Printf("Average length of call is %s minutes\n\n", projection.String())
+}
+
+func NewPhoneCallCharged(lengthOfCall int) PhoneCallCharged {
+	startTime, endTime := MakeCall(lengthOfCall)
+	costOfCall := float32(lengthOfCall) * 0.10
+
+	return PhoneCallCharged{costOfCall, startTime.UTC().Format(time.RFC3339), endTime.UTC().Format(time.RFC3339)}
+}
+
+func MakeCall(lengthOfCall int) (time.Time, time.Time) {
+
+	callDuration, _ := time.ParseDuration(fmt.Sprintf("%dm", lengthOfCall))
 
 	startTime := time.Now()
-	endTime := startTime.Add(lengthOfCall)
+	endTime := startTime.Add(callDuration)
 
 	return startTime, endTime
 }
